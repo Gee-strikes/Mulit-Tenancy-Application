5921601f76d0d7d79048f68eb673f68a65959741
Dipanshu Tihal <tihal.dipanshu@gmail.com>
2023-10-06 08:40:14 +0530

    New Feature - Multi Tenant

--- AspNetCoreMultitenant.Shared/Tenant.cs
+++ AspNetCoreMultitenant.Shared/Tenant.cs
@@ -1,6 +1,17 @@
-﻿namespace AspNetCoreMultitenant.Shared
+﻿﻿namespace AspNetCoreMultitenant.Shared
 {
     public class Tenant
     {
+        public int Id { get; set; }
+        public int DatabaseType { get; set; }
+        public string Host { get; set; }
+        public string ConnectionString { get; set; }
+        public string Name { get; set; }
+
+        public string StorageType { get; set; }
+        public string StorageConnectionString { get; set; }
+
+        public bool UseAdvancedProductThumbnails { get; set; }
+        public bool SendProductNotifications { get; set; }
     }
 }
